3c2be400574b793dd4517839ed1788c4bd231f65
brunnossanttos <brunossantosti99@gmail.com>
2023-11-02 23:10:28 -0300

    feat: :sparkles: todos endpoints criados

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get('/')
+  getAllPosts(): string {
+    //to do: add actual logic
+
+    return 'all posts';
+  }
+
+  @Get('/:postId')
+  getPostDetails(@Param('postId') postId: string) {
+    //to do: add actual logic
+
+    return `post with id = ${postId}`;
+  }
+
+  @Post('/')
+  createPost() {
+    //to do: add actual logic
+
+    return 'Create Post';
+  }
+
+  @Delete('/:postId')
+  deleteUser(@Param('postId') postId: string) {
+    //to do: add actual logic
+
+    return `delete post =  ${postId}!`;
+  }
+
+  @Put('/:postId/like')
+  likePost(@Param('postId') postId: string) {
+    //to do: add actual logic
+
+    return `like post =  ${postId}!`;
+  }
+
+  @Delete('/:postId/like')
+  unlikePost(@Param('postId') postId: string) {
+    //to do: add actual logic
+
+    return `unlike post =  ${postId}!`;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
@@ -9,10 +17,52 @@ export class UsersController {
     return 'all users';
   }
 
-  @Get('/:username')
-  getPostForHashtags(@Param() param): string {
+  @Get('/@{userName}')
+  getPostForHashtags(@Param('userName') userName: string): string {
     //to do: add actual logic
 
-    return `all users with username =  ${param.tag}!`;
+    return `all users with userName =  ${userName}!`;
+  }
+
+  @Get('/:userId')
+  getUserByUsername(@Param('userId') userId: string): string {
+    //to do: add actual logic
+
+    return `all users with userId =  ${userId}!`;
+  }
+
+  @Post('/')
+  createUser() {
+    //to do: add actual logic
+
+    return 'Create User';
+  }
+
+  @Patch('/:userId')
+  updateUserDetails(@Param('userId') userId: string) {
+    //to do: add actual logic
+
+    return `update details do user =  ${userId}!`;
+  }
+
+  @Put('/:userId/follow')
+  followUser(@Param('userId') userId: string) {
+    //to do: add actual logic
+
+    return `update follow user =  ${userId}!`;
+  }
+
+  @Delete('/:userId/follow')
+  deleteUser(@Param('userId') userId: string) {
+    //to do: add actual logic
+
+    return `delete follow user =  ${userId}!`;
+  }
+
+  @Get('/:userId/followers')
+  getFollowersOfUser(@Param('userId') userId: string) {
+    //to do: add actual logic
+
+    return `all followers for user =  ${userId}!`;
   }
 }
